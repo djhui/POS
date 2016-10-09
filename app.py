3e340d1d516ca19f0a38177aabcb7d14882821f7
djhui <cnJHDong@corp.fcs.int>
2016-10-09 16:05:30 +0800

    add nav bar

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import * 
  
 app = Flask(__name__)
- 
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 @app.route("/",methods=['POST','GET'])
  
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin123': 
-                error= "sorry"
+            session['name'] = request.form['username']
+            error= "sorry"
         else:
             return redirect(url_for('index'))
     return render_template('login.html',error=error)
@@ -16,8 +17,15 @@ def login():
  
 @app.route("/index")
 def index():
-    return render_template('index.html')
- 
+    
+    return render_template('index.html',session=session)
+
+@app.route("/base")
+def base():
+    return render_template('base.html',session=session)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
  
 if __name__ == "__main__":
     app.run(
