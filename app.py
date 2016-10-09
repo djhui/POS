8eb5dcf517920ceee7e9e7f44a879bd1e59c8b8c
JinHui <cnJHDong@corp.fcs.int>
2016-10-09 17:12:23 +0800

    change the base template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import * 
- 
+import sys
 app = Flask(__name__)
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 @app.route("/",methods=['POST','GET'])
@@ -7,17 +7,16 @@ app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin123': 
-            session['name'] = request.form['username']
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin': 
             error= "sorry"
         else:
+            session['name'] = request.form['username']
             return redirect(url_for('index'))
     return render_template('login.html',error=error)
  
  
 @app.route("/index")
 def index():
-    
     return render_template('index.html',session=session)
 
 @app.route("/base")
@@ -28,7 +27,13 @@ def page_not_found(e):
     return render_template('404.html'), 404
  
 if __name__ == "__main__":
+    if len(sys.argv)>1:
+        try:
+            int(sys.argv[1])
+            port = int(sys.argv[1])
+        except:
+            port = 5000
     app.run(
         host="0.0.0.0", 
-        port=5000, 
+        port=port, 
         debug=True)
