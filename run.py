fe5da598ead74930553605e3ac8f87f94c6e878b
JinHui <cnJHDong@corp.fcs.int>
2016-11-10 16:28:28 +0800

    add wsgi

--- run.py
+++ run.py
@@ -5,12 +5,5 @@ from app import app
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv)>1:
-        try:
-            int(sys.argv[1])
-            port = int(sys.argv[1])
-        except:pass
-    else:
-        port = 5000
-    app.run(host="0.0.0.0",port=port, debug=True)
+    app.run(host="0.0.0.0", debug=True)
     
--- run.wsgi
+++ run.wsgi
@@ -4,4 +4,3 @@ from app import app as YFapp
 #from flask_debug import Debug
 import sys
 sys.path.insert(0, '/opt/POS/')
-
