80fb11cff213a30bda11abe174fa002cc4825a18
djhui <cnJHDong@corp.fcs.int>
2016-11-11 13:28:46 +0800

    no comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import logging
-from logging.handlers import RotatingFileHandler
+#import logging
+#from logging.handlers import RotatingFileHandler
 
 #from flask.ext.openid import OpenID
 #from flask.ext.mail import Mail
@@ -19,12 +19,12 @@ lm.init_app(app)
 lm.login_view = 'login'
 
 
-file_handler = RotatingFileHandler('/var/log/YFapp.log', 'a', 1 * 1024 * 1024, 10)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.DEBUG)
-app.logger.info('App started')
+#file_handler = RotatingFileHandler('/var/log/YFapp.log', 'a', 1 * 1024 * 1024, 10)
+#file_handler.setLevel(logging.DEBUG)
+#file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+#app.logger.addHandler(file_handler)
+#app.logger.setLevel(logging.DEBUG)
+#app.logger.info('App started')
 
 
 
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-
-from app import app
 import sys
+sys.path.insert(0, '/opt/POS/')
+from app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80, debug=True)
+    app.run()
     
