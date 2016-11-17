077117f8257c56ad442f79d0c4d593c985fb3fea
djhui <cnJHDong@corp.fcs.int>
2016-11-17 12:32:50 +0800

    delete template and fix login issue

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-from flask import Flask
-import MySQLdb
-from flask_sqlalchemy import SQLAlchemy
-SECRET_KEY  = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
-SQLALCHEMY_DATABASE_URI =  'mysql://root:123456@127.0.0.1/flask'
+from hashlib import sha512
+from datetime import datetime
+SECRET_KEY = sha512(str(datetime.now())).hexdigest()
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/flask'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-#SERVER_NAME = ":80"
