9301ad2df6137fba1d3b40dd9ff09246dc7c57da
djhui <cnJHDong@corp.fcs.int>
2016-11-26 15:45:27 +0800

    update db file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 from hashlib import sha512
 from datetime import datetime
-#SECRET_KEY = sha512(str(datetime.now())).hexdigest()
-SECRET_KEY = "fff"
+SECRET_KEY = sha512(str(datetime.now())).hexdigest()
+#SECRET_KEY = "fff"
 SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/flask'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
