0853a073068584a2ea9e5ebdab1db6220f199c96
djhui <cnJHDong@corp.fcs.int>
2016-11-30 15:30:34 +0800

    update sales

--- app/views.py
+++ app/views.py
@@ -65,18 +65,26 @@ def sales():
     if request.method == 'POST':
         startdate = request.form['startdate']
         enddate = request.form['enddate']
-        CSE = request.form['CSE']
         SalesSum = []
-        if CSE == "all":
-            CSE = u"全店"
-            sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("orderdate<=:enddate","orderdate>=:startdate","offset=:offset").params(startdate=startdate,enddate=enddate,offset=0).first()
-            transfee = db.session.query(func.sum(Sales.Aprice)).filter("orderdate<=:enddate","orderdate>=:startdate","transportation not like :transportation","offset=:offset").params(startdate=startdate,enddate=enddate,transportation=u"%到付%",offset=0).first()
-        else:
+        sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("orderdate<=:enddate","orderdate>=:startdate","offset=:offset").params(startdate=startdate,enddate=enddate,offset=0).first()
+        transfee = db.session.query(func.sum(Sales.Aprice)).filter("orderdate<=:enddate","orderdate>=:startdate","transportation not like :transportation","offset=:offset").params(startdate=startdate,enddate=enddate,transportation=u"%到付%",offset=0).first()
+        price = sumre[0] + sumre[1]
+        result = {'id':1,'employee':u"全店情况",'price':"%.2f" % price,'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
+        SalesSum.append(result)
+        print SalesSum
+        userlist = User.query.all()
+        id=2
+        for CSE in userlist:
+            
+            CSE = u"%s" % CSE.nickname
             sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,offset=0).first()
             transfee = db.session.query(func.sum(Sales.Aprice)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","transportation not like :transportation","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,transportation=u"%到付%",offset=0).first()
-        price = sumre[0] + sumre[1]
-        result = {'id':'id','employee':CSE,'price':"%.2f" % price,'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
-        SalesSum.append(result)
+            try:
+                price = sumre[0] + sumre[1]
+                result = {'id':id,'employee':CSE,'price':"%.2f" % price,'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
+                SalesSum.append(result)
+            except:pass
+            id +=1
         return json.dumps({'msg':SalesSum})
     nickname=User.query.order_by(User.username)
     return render_template('sales.html',session=session,nav = u"销售总览",catelist=g.catelist,nickname=nickname)
