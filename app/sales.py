2cffc83b0203fc3c1ff8220b343f0eb926a446f1
Dong Desmond <Dong Desmond>
2017-02-06 20:54:05 +0800

    add sales calculate

--- app/sales.py
+++ app/sales.py
@@ -14,28 +14,49 @@ from dateutil.relativedelta import relativedelta
 @login_required
 def sales():
     if request.method == 'POST':
-        startdate = request.form['startdate']
-        enddate = request.form['enddate']
-        SalesSum = []
-        sumre = db.session.query(func.sum(Sales.price), func.sum(Sales.advprice), func.sum(Sales.Recashes)).filter("orderdate<=:enddate", "orderdate>=:startdate", "offset=:offset").params(startdate=startdate, enddate=enddate, offset=0).first()
-        transfee = db.session.query(func.sum(Sales.Aprice)).filter("orderdate<=:enddate", "orderdate>=:startdate", "transportation not like :transportation", "offset=:offset").params(startdate=startdate, enddate=enddate, transportation=u"%到付%", offset=0).first()
-        price = sumre[0] + sumre[1]
-        result = {'id':1, 'employee':u"全店情况", 'price':"%.2f" % price, 'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
-        SalesSum.append(result)
-        userlist = User.query.all()
-        id=2
-        for CSE in userlist:
-            CSE = u"%s" % CSE.nickname
-            try:
-                sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,offset=0).first()
-                transfee = db.session.query(func.sum(Sales.Aprice)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","transportation not like :transportation","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,transportation=u"%到付%",offset=0).first()
-            
-                price = sumre[0] + sumre[1]
-                result = {'id':id,'employee':CSE,'price':"%.2f" % price,'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
-                SalesSum.append(result)
-            except:pass
-            id +=1
-        return json.dumps({'msg':SalesSum})
+        submit = request.form['submit']
+        if submit == "area":
+            startdate = request.form['startdate']
+            enddate = request.form['enddate']
+            SalesSum = []
+            sumre = db.session.query(func.sum(Sales.price), func.sum(Sales.advprice), func.sum(Sales.Recashes)).filter("orderdate<=:enddate", "orderdate>=:startdate", "offset=:offset").params(startdate=startdate, enddate=enddate, offset=0).first()
+            transfee = db.session.query(func.sum(Sales.Aprice)).filter("orderdate<=:enddate", "orderdate>=:startdate", "transportation not like :transportation", "offset=:offset").params(startdate=startdate, enddate=enddate, transportation=u"%到付%", offset=0).first()
+            price = sumre[0] + sumre[1]
+            result = {'id':1, 'employee':u"全店情况", 'price':"%.2f" % price, 'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
+            SalesSum.append(result)
+            userlist = User.query.all()
+            id = 2
+            for CSE in userlist:
+                CSE = u"%s" % CSE.nickname
+                try:
+                    sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,offset=0).first()
+                    transfee = db.session.query(func.sum(Sales.Aprice)).filter("CSE=:CSE","orderdate<=:enddate","orderdate>=:startdate","transportation not like :transportation","offset=:offset").params(CSE=CSE,startdate=startdate,enddate=enddate,transportation=u"%到付%",offset=0).first()
+                    price = sumre[0] + sumre[1]
+                    result = {'id':id, 'employee':CSE, 'price':"%.2f" % price, 'Aprice':"%.2f" %transfee[0], 'Recashes':"%.2f" % sumre[2]}
+                    SalesSum.append(result)
+                except:pass
+                id += 1
+            return json.dumps({'msg':SalesSum})
+        if submit == "cal":
+            caldate = request.form['caldate']
+            SalesSum = []
+            sumre = db.session.query(func.sum(Sales.price), func.sum(Sales.advprice), func.sum(Sales.Recashes)).filter("Commission=:caldate", "offset=:offset").params(caldate=caldate,  offset=0).first()
+            transfee = db.session.query(func.sum(Sales.Aprice)).filter("Commission=:caldate", "transportation not like :transportation", "offset=:offset").params(caldate=caldate, transportation=u"%到付%", offset=0).first()
+            price = sumre[0] + sumre[1]
+            result = {'id':1, 'employee':u"全店情况", 'price':"%.2f" % price, 'Aprice':"%.2f" %transfee[0] ,'Recashes':"%.2f" % sumre[2]}
+            SalesSum.append(result)
+            userlist = User.query.all()
+            id = 2
+            for CSE in userlist:
+                CSE = u"%s" % CSE.nickname
+                try:
+                    sumre = db.session.query(func.sum(Sales.price),func.sum(Sales.advprice),func.sum(Sales.Recashes)).filter("CSE=:CSE","Commission=:caldate", "offset=:offset").params(CSE=CSE, caldate=caldate, offset=0).first()
+                    transfee = db.session.query(func.sum(Sales.Aprice)).filter("CSE=:CSE","Commission=:caldate", "transportation not like :transportation","offset=:offset").params(CSE=CSE,caldate=caldate, transportation=u"%到付%",offset=0).first()
+                    price = sumre[0] + sumre[1]
+                    result = {'id':id, 'employee':CSE, 'price':"%.2f" % price, 'Aprice':"%.2f" %transfee[0], 'Recashes':"%.2f" % sumre[2]}
+                    SalesSum.append(result)
+                except:pass
+                id += 1
+            return json.dumps({'msg':SalesSum})
     nickname=User.query.order_by(User.username)
-    return render_template('sales.html',session=session,nav = u"销售总览",catelist=g.catelist,nickname=nickname)
-#---------------------------------------------------------------------------------------
+    return render_template('sales.html', session=session, nav = u"销售总览", catelist=g.catelist, nickname=nickname)
