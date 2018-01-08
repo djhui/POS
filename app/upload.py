4dc9af4439f0490022a510b2fc0bca8d488d7d76
djhui <djhui@live.cn>
2018-01-08 19:46:26 +0800

    update upload file functions

--- app/upload.py
+++ app/upload.py
@@ -18,7 +18,26 @@ def upload_file():
         ext = f.filename.split(".")[-1]
         filename = md5(str(datetime.now())).hexdigest() + "." + ext
         minetype = f.content_type
-        if ext.lower() not in ['jpg','jpeg','png','bmp','gif','xls','xlsx']:
+        if ext.lower() not in ['jpg','jpeg','png','bmp','gif']:
+            return json.dumps({"files": [{"name": u"文件格式错误,请上传允许的格式", "minetype": minetype}]})
+        else:    
+            #filename1 = './app/static/upload/%s' % (filename)
+            filename1 = '/opt/POS/app/static/upload/%s' % (filename)
+            f.save(filename1) 
+            log = u"上传文件%s" %filename
+            db.session.add(Logs(log,u"上传文件",session['nickname'])) 
+            db.session.commit()
+            return json.dumps({"files": [{"name": filename, "minetype": minetype}]})
+
+@app.route('/uploadfile', methods=['GET', 'POST'])
+@login_required
+def uploadfile():
+    if request.method == 'POST':
+        f = request.files['files[]']
+        ext = f.filename.split(".")[-1]
+        filename = f.filename
+        minetype = f.content_type
+        if ext.lower() not in ['xls','xlsx','doc','docx','ppt','pptx']:
             return json.dumps({"files": [{"name": u"文件格式错误,请上传允许的格式", "minetype": minetype}]})
         else:    
             #filename1 = './app/static/upload/%s' % (filename)
