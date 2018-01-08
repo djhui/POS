57f65f2f2642cc66a0c54f5b3ba9ac61085efb8f
djhui <djhui@live.cn>
2018-01-08 20:10:58 +0800

    reload utf-8

--- run.py
+++ run.py
@@ -3,6 +3,9 @@
 import sys
 sys.path.insert(0, '/opt/POS/')
 from app import app as application
+reload(sys)
+sys.setdefaultencoding("utf8")
+
 
 if __name__ == "__main__":
     application.run(debug=True)
