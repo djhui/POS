a8145c4237ff082daec97593708542b33598346c
djhui <djhui@live.cn>
2018-01-08 19:59:45 +0800

    no comments

--- app/upload.py
+++ app/upload.py
@@ -36,10 +36,10 @@ def uploadfile():
         f = request.files['files[]']
         ext = f.filename.split(".")[-1]
         try:
-            filename = f.filename.decode('gb2312')
+            filename = f.filename.encode('gb2312')
         except UnicodeDecodeError:
             
-            filename = f.filename.decode('utf-8')
+            filename = f.filename.encode('utf-8')
         minetype = f.content_type
         if ext.lower() not in ['xls','xlsx','doc','docx','ppt','pptx']:
             return json.dumps({"files": [{"name": u"文件格式错误,请上传允许的格式", "minetype": minetype}]})
